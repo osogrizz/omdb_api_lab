a32e7422a3cda922d2fa194ce12d6a5cec058b02
Leo Torres <leo.htor@gmail.com>
2015-01-28 19:40:27 -0800

    search title function

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
+$.ajaxSetup({
+	
+	data: {
+		access_token: "ed810296b2eb2083a4e3239b2bc828a3aa109408"
+	}
+});
+
+
+
+
 $(document).ready (function () {
+	$("form").on("submit", function(e) {
+		
+		e.preventDefault();
+		var title = e.target.movieTitle.value;
+		console.log(title);
+		$.getJSON("http://www.omdbapi.com/?t="  + title,  function(data) {
+			console.log(data);
+			$("body").append("<li>" +  data.Title, "<br>" +  data.Genre,"<br>" + data.Language + "</li>");
+
+			if (Response === false) {
+				$("body").append(Error);
+			}
+
+	});
+
+  });
 
 });
+
+
+// key ed810296b2eb2083a4e3239b2bc828a3aa109408
